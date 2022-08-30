17c3b607f2f1f115b61dc5abd41ef4c319aed0d8
Minso5 <109052010+Minso5@users.noreply.github.com>
2022-08-30 11:03:37 +0900

    Add files via upload

---
+++ src/Exam01.java
@@ -0,0 +1,13 @@
+public class Exam01 {
+
+	public static void main(String[] args) {
+		int size = 27;
+		String name = "Fido";
+		//Dog myDog = new Dog(name, size);
+		int[] nemList = {2,4,6,8};
+		
+		System.out.print(size);
+		System.out.println(name);
+		System.out.println(numList[2]);
+	}
+}
